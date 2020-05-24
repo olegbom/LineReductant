ec98b4546544aabc6da7320cc4361725574841f5
Высоцкий Олег <Olegbom@gmail.com>
2020-05-24 23:06:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TextCopy;
+
+namespace LineReductant
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            Console.WriteLine("Для выхода нажмите Ctrl + C\n");
+            
+            while (!isExit)
+            {
+                var info = Console.ReadKey();
+
+                if (info.Key == ConsoleKey.V)
+                {
+                    Clipboard.SetText(Clipboard.GetText().Replace(Environment.NewLine, " "));
+                    Console.WriteLine("Текст в буффере обмена изменён!\n");
+                }
+                else
+                {
+                    isExit = (info.Modifiers & ConsoleModifiers.Control) != 0 &&
+                             info.Key == ConsoleKey.C;
+                }
+            }
+        }
+
+    }
+    
+}
